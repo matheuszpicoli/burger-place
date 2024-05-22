b8b8da267e94e9a1781ce52101601c1416aabe5a
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-05-22 17:58:29 -0300

    adding the counter to the button that goes to the cart

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 //- React
-import React from "react"
+import React, { useState, useLayoutEffect } from "react"
 
 //- React Router DOM
 import { Link } from "react-router-dom"
@@ -11,23 +11,35 @@ import { MdShoppingCart as ShoppingCart } from "react-icons/md"
 export default function Footer({
 	location
 }) {
+	const [cartItemsCount, setCartItemsCount] = useState(0)
+
+	useLayoutEffect(() => {
+		const savededInCart = localStorage.getItem("cart")
+		const cartItems = savededInCart ? JSON.parse(savededInCart) : []
+
+		setCartItemsCount(cartItems.length)
+	}, [])
+
 	return (
 		<div className="fixed bottom-0 w-full backdrop-blur text-white/60 flex justify-around items-center h-8 cursor-default">
 			<div className="flex items-center">
 				<Location className="w-6 h-6 mr-2" />
 				<i>{location}</i>
 			</div>
-			<Link
-				to={"/cart"}
-				className="hover:text-white transition duration-300 active:opacity-20"
-			>
-				<ShoppingCart className="inline w-6 h-6 mr-2" />
-				Ver carrinho
-				<sup className="p-1 m-1 font-bold">
-					{/* Valor fictício, alterar depois */}
-					1 item
-				</sup>
-			</Link>
+
+			{cartItemsCount > 0 && (
+				<Link
+					to={"/cart"}
+					className="hover:text-white transition duration-300 active:opacity-20"
+				>
+					<ShoppingCart className="inline w-6 h-6 mr-2" />
+					Ver carrinho
+					<sup className="p-1 m-1 font-bold">
+						{cartItemsCount === 1 && `${cartItemsCount} item`}
+						{cartItemsCount > 1 && `${cartItemsCount} itens`}
+					</sup>
+				</Link>
+			)}
 		</div>
 	)
 }
