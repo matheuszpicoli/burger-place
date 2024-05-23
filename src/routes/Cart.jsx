fa359837761b05e3b9dfc8b58284330bd4e9abd0
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-05-23 17:17:37 -0300

    only shows the cart interface if it has an item

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -55,81 +55,94 @@ export default function Cart() {
 
 			<main className="bg-stone-700 flex flex-row min-h-screen flex-grow">
 
-				<div className="flex flex-col w-1/4">
-					<h1 className="flex items-center ml-3 w-full text-white tracking-wide text-lg font-bold pb-4 pt-4 cursor-default">
-						<Pencil className="inline w-5 h-5" />
-						<span className="align-middle ml-1">
-							Revise seu pedido
-						</span>
-					</h1>
+				{cart.length <= 0 ? (
+					<div className="flex justify-center w-full animate-fade-in">
+						<h1 className="text-white tracking-wide text-2xl font-bold pt-4 cursor-default">
+							<ShoppingCart className="inline w-7 h-7" />
+							<span className="align-middle ml-1">
+								Seu carrinho está vazio
+							</span>
+						</h1>
+					</div>
+				) : (
+					<React.Fragment>
+						<div className="flex flex-col w-1/4">
+							<h1 className="flex items-center ml-3 w-full text-white tracking-wide text-lg font-bold pb-4 pt-4 cursor-default">
+								<Pencil className="inline w-5 h-5" />
+								<span className="align-middle ml-1">
+									Revise seu pedido
+								</span>
+							</h1>
 
-					{cart.map((item, index) => {
-						const totalExtras = item.extras.reduce((extraSum, extra) => extraSum + extra.price, 0)
+							{cart.map((item, index) => {
+								const totalExtras = item.extras.reduce((extraSum, extra) => extraSum + extra.price, 0)
 
-						return (
-							<div
-								key={index}
-								className="flex flex-col bg-white border-y-2 border-x-2 rounded-l rounded-r m-3 p-3 cursor-default hover:bg-gradient-to-b from-black/10 via-black/10 to-transparent animate-fade-in"
-							>
-								<h2 className="uppercase tracking-wide font-medium flex justify-between">
-									{item.description}
-
-									<button
-										onClick={() => removeFromCart(index)}
-										className="text-red-600 opacity-60 hover:opacity-100 active:opacity-60 transition duration-300"
+								return (
+									<div
+										key={index}
+										className="flex flex-col bg-white border-y-2 border-x-2 rounded-l rounded-r m-3 p-3 cursor-default hover:bg-gradient-to-b from-black/10 via-black/10 to-transparent animate-fade-in"
 									>
-										<Close className="inline w-5 h-5" />
-										<span className="text-xs ml-1 align-middle italic">
-											Remover
-										</span>
-									</button>
-								</h2>
+										<h2 className="uppercase tracking-wide font-medium flex justify-between">
+											{item.description}
 
-								<p className="text-green-600 font-semibold">
-									{maskForPrice(item.price)}
-								</p>
-								<small>
-									{item.extras.map((extra, index) => {
-										let displayName = extra.name
-
-										if (index > 0) displayName = displayName.charAt(0).toLowerCase().concat(displayName.slice(1))
-
-										return (
-											<span
-												key={index}
-												className="inline"
+											<button
+												onClick={() => removeFromCart(index)}
+												className="text-red-600 opacity-60 hover:opacity-100 active:opacity-60 transition duration-300"
 											>
-												{displayName}{index !== item.extras.length - 1 ? ", " : "."}
-											</span>
-										)
-									})}
-								</small>
-								<div className="p-px bg-slate-300 mt-1 mb-1 rounded-l rounded-r"></div>
-								{totalExtras !== 0 && (
-									<small className="block font-medium">
-										Complementos: <span className="text-yellow-600 underline">{maskForPrice(totalExtras)}</span>
-									</small>
-								)}
-								{item.observation && (
-									<small>
-										<span className="font-medium">Observações do pedido: </span>{item.observation}
-									</small>
-								)}
-							</div>
-						)
-					})}
-				</div>
+												<Close className="inline w-5 h-5" />
+												<span className="text-xs ml-1 align-middle italic">
+													Remover
+												</span>
+											</button>
+										</h2>
 
-				<div className="flex flex-col w-9/12">
-					<h1 className="flex items-center ml-3 text-white tracking-wide text-lg font-bold pb-4 pt-4 cursor-default">
-						<Confirm className="inline w-5 h-5" />
-						<span className="align-middle ml-1">
-							Complete seu pedido
-						</span>
-					</h1>
+										<p className="text-green-600 font-semibold">
+											{maskForPrice(item.price)}
+										</p>
+										<small>
+											{item.extras.map((extra, index) => {
+												let displayName = extra.name
 
-					<Form />
-				</div>
+												if (index > 0) displayName = displayName.charAt(0).toLowerCase().concat(displayName.slice(1))
+
+												return (
+													<span
+														key={index}
+														className="inline"
+													>
+														{displayName}{index !== item.extras.length - 1 ? ", " : "."}
+													</span>
+												)
+											})}
+										</small>
+										<div className="p-px bg-slate-300 mt-1 mb-1 rounded-l rounded-r"></div>
+										{totalExtras !== 0 && (
+											<small className="block font-medium">
+												Complementos: <span className="text-yellow-600 underline">{maskForPrice(totalExtras)}</span>
+											</small>
+										)}
+										{item.observation && (
+											<small>
+												<span className="font-medium">Observações do pedido: </span>{item.observation}
+											</small>
+										)}
+									</div>
+								)
+							})}
+						</div>
+
+						<div className="flex flex-col w-9/12">
+							<h1 className="flex items-center ml-3 text-white tracking-wide text-lg font-bold pb-4 pt-4 cursor-default">
+								<Confirm className="inline w-5 h-5" />
+								<span className="align-middle ml-1">
+									Complete seu pedido
+								</span>
+							</h1>
+
+							<Form />
+						</div>
+					</React.Fragment>
+				)}
 			</main>
 		</React.Fragment>
 	)
