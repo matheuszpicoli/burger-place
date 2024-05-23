ebcfb7c2b44fc91ea2261d2ae1f03dcefa38be92
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-05-23 09:45:57 -0300

    slight component improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,6 +27,14 @@ export default function Footer({
 		return () => clearTimeout(popoverTime)
 	}, [])
 
+	const date = new Date()
+	const hours = date.getHours()
+
+	const openingTime = 18
+	const closingTime = 23
+
+	const isOpen = hours >= openingTime && hours < closingTime
+
 	return (
 		<div className="fixed bottom-0 w-full backdrop-blur text-white/60 flex justify-around items-center h-8 cursor-default">
 			<div className="flex items-center">
@@ -35,7 +43,7 @@ export default function Footer({
 			</div>
 
 
-			{cartItemsCount > 0 && (
+			{isOpen && cartItemsCount > 0 && (
 				<React.Fragment>
 					{showPopover && (
 						<div className="fixed bottom-10 bg-white right-32 text-black p-3 rounded-l rounded-r bg-opacity-60 animate-fade-in">
@@ -48,9 +56,8 @@ export default function Footer({
 					>
 						<ShoppingCart className="inline w-6 h-6 mr-2" />
 						Ver carrinho
-						<sup className="p-1 m-1 font-bold">
-							{cartItemsCount === 1 && `${cartItemsCount} item`}
-							{cartItemsCount > 1 && `${cartItemsCount} itens`}
+						<sup className="p-1 m-1 font-thin">
+							{cartItemsCount === 1 ? `${cartItemsCount} item` : `${cartItemsCount} itens`}
 						</sup>
 					</Link>
 				</React.Fragment>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,14 +81,14 @@ export function Operation() {
 
 			<p>
 				{isOpen && (
-					<React.Fragment>
+					<div className="cursor-default">
 						<Clock className="inline m-2" /> <b>60-100min</b> • Mínimo <span className="text-green-400">R$20,00</span>.
-					</React.Fragment>
+					</div>
 				)}
 				{!isOpen && (
-					<React.Fragment>
+					<div className="cursor-default">
 						Abre às <span className="text-red-400">{openingTime}h</span>, fecha às <span className="text-red-400">{closingTime}h</span>.
-					</React.Fragment>
+					</div>
 				)}
 			</p>
 		</React.Fragment>
